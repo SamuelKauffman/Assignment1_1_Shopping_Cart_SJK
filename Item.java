68b18396081ba8f847621a6faf5a1de328ebbb7e
Sam Kauffman <Samueljkauff@gmail.com>
2023-09-14 11:54:29 -0400

    javadocs

--- Item.java
+++ Item.java
@@ -1,35 +1,53 @@
 /**
- * A concrete class representing an item that can be stored in a shopping cart.
+ * @author sam KAuffman
+ * @version 1.0
  */
 public class Item {
 
     private String name;
     private double price;
 
+    /**
+     * @param name
+     * @param price
+     */
     public Item(String name, double price) {
         this.name = name;
         this.price = price;
     }
     
+    /**
+     * @return
+     */
     public String getName() {
         return name;
     }
     
+    /**
+     * @param item name
+     */
     public void setName(String name) {
     	this.name = name;
     }
 
+    /**
+     * @return item price
+     */
     public double getPrice() {
         return price;
     }
     
+    /**
+     * @param item price
+     */
     public void setPrice(double price) {
     	this.price = price;
     }
     
+    /**
+     * @return items name
+     */
     public String toString() {
     	return name;
     }
-
-    // You can add more methods and fields as needed for your items.
 }
